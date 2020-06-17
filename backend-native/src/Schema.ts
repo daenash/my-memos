01deca6e7cf434ad6f1dc5f5869b6e1a2abf2a27
Gutai Dénes <daenash@gmail.com>
2020-06-17 21:26:43 +0200

    remove legacy

--- backend-native/src/Schema.ts
+++
@@ -1,8 +0,0 @@
-import { GraphQLSchema } from "graphql";
-import Query from "./models/Query";
-import Mutation from "./models/Mutation";
-
-export default new GraphQLSchema({
-  query: Query,
-  mutation: Mutation,
-});
